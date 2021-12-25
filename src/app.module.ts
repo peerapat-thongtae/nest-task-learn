1b022c131631b406aa03436b6bac4fb5d73a52b1
peerapat-thongtae <peera.thongtae@gmail.com>
2021-12-25 21:35:01 +0700

    typeorm connect postgres & task entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeormConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/dist/typeorm.config.js
@@ -0,0 +1,13 @@
+"use strict";
+exports.__esModule = true;
+exports.typeormConfig = void 0;
+exports.typeormConfig = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'user',
+    password: '',
+    database: 'taskmanagement',
+    entities: [__dirname + '/../**/*.entity.ts'],
+    synchronize: true
+};
